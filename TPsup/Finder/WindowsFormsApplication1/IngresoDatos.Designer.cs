023490e35f85256565ab41665c301e24ce89e971
Dragonflares <ivanlose@hotmail.com>
2019-11-14 00:23:17 -0300

    A ver

--- TPsup/Finder/WindowsFormsApplication1/IngresoDatos.Designer.cs
+++ TPsup/Finder/WindowsFormsApplication1/IngresoDatos.Designer.cs
@@ -36,11 +36,20 @@
             this.puntoY = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
             this.SuspendLayout();
             // 
@@ -76,7 +85,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(291, 65);
+            this.button1.Location = new System.Drawing.Point(294, 63);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 23);
             this.button1.TabIndex = 2;
@@ -86,16 +95,16 @@
             // 
             // puntoX
             // 
-            this.puntoX.Location = new System.Drawing.Point(46, 65);
+            this.puntoX.Location = new System.Drawing.Point(46, 27);
             this.puntoX.Name = "puntoX";
-            this.puntoX.Size = new System.Drawing.Size(100, 20);
+            this.puntoX.Size = new System.Drawing.Size(75, 20);
             this.puntoX.TabIndex = 3;
             // 
             // puntoY
             // 
-            this.puntoY.Location = new System.Drawing.Point(178, 65);
+            this.puntoY.Location = new System.Drawing.Point(178, 27);
             this.puntoY.Name = "puntoY";
-            this.puntoY.Size = new System.Drawing.Size(100, 20);
+            this.puntoY.Size = new System.Drawing.Size(75, 20);
             this.puntoY.TabIndex = 4;
             // 
             // label1
@@ -117,28 +126,9 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Punto y:";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(319, 298);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Continuar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(46, 298);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Atras";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(178, 298);
+            this.button4.Location = new System.Drawing.Point(178, 63);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 9;
@@ -162,15 +152,125 @@
             this.label4.TabIndex = 11;
             this.label4.Text = "Metodo:";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Resultado";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(46, 348);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(207, 72);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Enter += new System.EventHandler(this.button3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Mostrar pasos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 306);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(207, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(301, 400);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(302, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 30);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Calcular un K\r\nen el polinomio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(298, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Valor de K (en x):";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(298, 453);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 15);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Resultado";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(301, 471);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 22;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(301, 426);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Calcular";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // IngresoDatos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 364);
+            this.ClientSize = new System.Drawing.Size(441, 527);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -197,10 +297,19 @@
         private System.Windows.Forms.TextBox puntoY;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
     }
 }
--- TPsup/Finder/WindowsFormsApplication1/IngresoDatos.cs
+++ TPsup/Finder/WindowsFormsApplication1/IngresoDatos.cs
@@ -13,13 +13,24 @@ namespace WindowsFormsApplication1
     public partial class IngresoDatos : Form
     {
         private String metodo;
-          
+        private int[][] matriz;
+        private Boolean pasosmostrados = false;
 
         public IngresoDatos(String _metodo)
         {
             InitializeComponent();
             metodo = _metodo;
             textBox1.Text = metodo;
+            label3.Visible = false;
+            button3.Visible = false;
+            listView1.Visible = false;
+            textBox2.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            button5.Visible = false;
         }
 
         private void IngresoDatos_Load(object sender, EventArgs e)
@@ -81,9 +92,61 @@ namespace WindowsFormsApplication1
                 matrizXY[i] = auxiliar;
                 i++; 
             }
-            new Procesamiento(matrizXY).Show(this);
+            matriz = matrizXY;
+            label3.Visible = true;
+            button3.Visible = true;
+            textBox2.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            button5.Visible = true;
+            Lagrange lagrangecalculator = new Lagrange();
+            textBox2.Text = lagrangecalculator.Calcular(matrizXY);
+            button2.Text = "Recalcular";
 
             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pasosmostrados)
+            {
+                listView1.Visible = false;
+                foreach(ListViewItem item in listView1.Items)
+                {
+                    listView1.Items.Remove(item);
+                }
+                pasosmostrados = false;
+            }
+            else
+            {
+                listView1.Visible = true;
+                Lagrange lagrangecalculator = new Lagrange();
+                List<string> lis = lagrangecalculator.ListaLis(matriz);
+                foreach (string li in lis)
+                {
+                    ListViewItem some = new ListViewItem(li);
+                    listView1.Items.Add(some);
+                }
+                pasosmostrados = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Lagrange lagrangecalculator = new Lagrange();
+            if(textBox4.Text == "")
+            {
+                new AdvertenciaXinvalido("Usted ingresó no ingresó un valor de k.").Show(this);
+            }
+            else
+            {
+                int k = (int)Convert.ToInt64(textBox4.Text);
+                textBox3.Text = lagrangecalculator.CalcularEnPunto(k, matriz).ToString();
+            }
+
         }
     }
 }
